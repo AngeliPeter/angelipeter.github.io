dad0c76280b336f4984139921b0acc318414bbbf
Angeli Péter <angelipeter52@gmail.com>
2021-11-28 23:39:00 +0100

    albums

--- spotify/Data/Spotify.cs
+++ spotify/Data/Spotify.cs
@@ -62,11 +62,13 @@ namespace spotify.Data
 
             if (PKCE is null)
             {
+                Console.WriteLine("PKCE NULL");
                 return (false, RequestNewToken(), null);
             }
 
             if (DateTime.UtcNow > (PKCE.CreatedAt.AddSeconds(PKCE.ExpiresIn - 60)))
             {
+                Console.WriteLine("PKCE EXPIRED");
                 try
                 {
                     var newResponse = await new OAuthClient().RequestToken(new PKCETokenRefreshRequest(ClientId,
@@ -76,11 +78,13 @@ namespace spotify.Data
                 }
                 catch
                 {
+                    Console.WriteLine("PKCE CATCH");
                     return (false, RequestNewToken(), null);
                 }
             }
             else
             {
+                Console.WriteLine("PKCE NOT EXPIRED");
                 if (!check)
                 {
                     SpotifyObject = new SpotifyClient(PKCE.AccessToken);
